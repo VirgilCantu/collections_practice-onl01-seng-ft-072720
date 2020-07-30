6ee55807a731f1e172b300444f02bc18652c86a4
Virgil Cantu <cantu.sampsom@gmail.com>
2020-07-30 21:26:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,38 @@
+require 'pry'
 
+
+def sort_array_asc(interger_array)
+  interger_array.sort
+end
+
+
+def sort_array_desc(interger_array)
+  interger_array.sort.reverse
+end
+
+
+def sort_array_char_count(string_array)
+  string_array.sort do |a, b|
+  a.length <=> b.length
+  end
+end
+
+
+def swap_elements(array)
+  array [0], array[1], array[2] = array [0], array[2], array [1]
+end
+
+
+def reverse_array(interger_array)
+  interger_array.reverse
+end
+
+
+def kesha_maker(string_array)
+  kesha_strings = []
+  string_array.each do |name|
+    kesha_strings << name[4] << "$"
+    binding.pry
+  end
+  kesha_strings
+end
